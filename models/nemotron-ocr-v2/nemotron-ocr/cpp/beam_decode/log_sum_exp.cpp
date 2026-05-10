9df00a7b7a42fdc377529915f2d4b7c5f644d223
gaiavn-dev <gaiavn.agency@gmail.com>
2026-05-10 22:54:31 +0200

    chore: finalize transition to Ollama-exclusive architecture and update documentation v2.2

--- models/nemotron-ocr-v2/nemotron-ocr/cpp/beam_decode/log_sum_exp.cpp
+++
@@ -1,6 +0,0 @@
-// SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-#include "log_sum_exp.h"
-
-const float_t NEG_INF = -std::numeric_limits<float_t>::infinity();
